21f5d96c6d708260c493ad42b2f5c74e0fa8ef14
littleflow3r <36025078+littleflow3r@users.noreply.github.com>
2025-03-19 12:14:11 +0900

    add sampling methods

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from algs.pc import pc
 from algs.ges import ges
 from algs.pairwise import llm_pairwise , midllm_pairwise
 from algs.bfs import llm_bfs
-from algs.dagma_baseline.dagma_wrapper import dagma_nonlinear_wrapper, dagma_linear_wrapper
+from algs.dagma_wrapper import dagma_nonlinear_wrapper, dagma_linear_wrapper
 import numpy as np
 from metrics import compute_metrics
 from llm_model import load_models
@@ -28,19 +28,28 @@ def write_metric(adj_mat, predicted_adj_mat, logdir, dataset, n_samples, alg):
       json.dump(metrics, outfile)
 
 def data_sampling(df, size, seed, sample_method):
+  np.random.seed(seed)
   if sample_method == 'systematic':
     df_sampled = df.iloc[::10] #step=10 pool/size n
+    
   elif sample_method == 'cluster':
-    df['cluster'] = np.random.randint(0, 10, size=1000)  # Assigning clusters (10 clusters: 0-9)
+    df['cluster'] = np.random.randint(0, 10, size=len(df))  # Assigning clusters (10 clusters: 0-9)
     # Step 1: Take 10 samples from each cluster
-    df_sampled = df.groupby('cluster').apply(lambda x: x.sample(n=10, random_state=seed)).reset_index(drop=True)
+    # df_sampled = df.groupby('cluster').apply(lambda x: x.sample(n=10, random_state=seed)).reset_index(drop=True)
+    df_sampled = df.groupby('cluster', group_keys=False).apply(lambda x: x.sample(n=min(len(x), 10), random_state=seed))
+    df_sampled = df_sampled.drop(columns=['cluster'])
+
   elif sample_method == 'adaptive':
     num_clusters = 10
     kmeans = KMeans(n_clusters=num_clusters, random_state=seed, n_init=10)
     df['cluster'] = kmeans.fit_predict(df)  # Use relevant numerical columns
-    df_sampled = df.groupby('cluster').apply(lambda x: x.sample(n=10, random_state=seed)).reset_index(drop=True)
+    # df_sampled = df.groupby('cluster').apply(lambda x: x.sample(n=10, random_state=seed)).reset_index(drop=True)
+    df_sampled = df.groupby('cluster', group_keys=False).apply(lambda x: x.sample(n=min(len(x), 10), random_state=seed))
+    df_sampled = df_sampled.drop(columns=['cluster'])
+
   else:
     df_sampled = df.sample(n=size, random_state=seed)
+  
   return df_sampled
 
 args = get_args()
@@ -58,15 +67,15 @@ elif args.dataset in ["sprinkler", "titanic", "sachs", "auto"]:
   adj_mat = dag['adjmat'].to_numpy().astype(int)
   df = bn.sampling(dag, n=args.pool_size)
 else:
-  dag = bn.import_DAG(f'./data/{args.dataset}.bif')
+  dag = bn.import_DAG(f'data/{args.dataset}.bif')
   var_names = dag['adjmat'].columns
   adj_mat = dag['adjmat'].to_numpy().astype(int)
   df = bn.sampling(dag, n=args.pool_size)
 
 df = data_sampling(df, args.n_samples, args.seed, args.sample_method)
 data = df.to_numpy()
-# print (f'{df.shape=}')
-# print (df.head())
+print (df.columns, df.shape)
+print (df.head())
 # print (df.columns.tolist(), len(df.columns.tolist()))
 # print (adj_mat)
 # sys.exit()
@@ -77,7 +86,6 @@ data = df.to_numpy()
 # 400     1    1      0     1      0       1     1     0
 # 15      1    1      1     1      1       1     1     1
 
-# np.random.seed(args.seed)
 gaussian_noise = np.random.normal(loc=0.0, scale=0.00001, size=data.shape)
 
 if args.alg == 'stat_all':
@@ -99,21 +107,22 @@ if args.alg == 'stat_all':
   # write_metric(adj_mat, predicted_adj_mat_dl, args.logdir, args.dataset, args.n_samples, 'dagma_linear')
 
 elif args.alg == 'llm_obs':
-  print (f'\nRunning LLM BFS with statistics...')
-  predicted_adj_mat_bfsobs = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df, args.temp, include_statistics=True, include_obs=False)
-  write_metric(adj_mat, predicted_adj_mat_bfsobs, args.logdir, args.dataset, args.n_samples, f'llm{args.temp}_bfs_with_stat')
-
-  print (f'\nRunning LLM pairwise with statistics...')
-  predicted_adj_mat_llmpobs = llm_pairwise(VAR_NAMES_AND_DESC[args.dataset], prompts[args.dataset], df, args.temp, include_statistics=True)
-  write_metric(adj_mat, predicted_adj_mat_llmpobs, args.logdir, args.dataset, args.n_samples, f'llm{args.temp}_pairwise_with_stat')
-
   print (f'\nRunning LLM BFS with obs...')
-  predicted_adj_mat_bfsobs = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df, args.temp, include_statistics=True, include_obs=True)
+  predicted_adj_mat_bfsobs = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df, args.temp, include_statistics=False, include_obs=True)
   write_metric(adj_mat, predicted_adj_mat_bfsobs, args.logdir, args.dataset, args.n_samples, f'llm{args.temp}{args.sample_method}_bfs_with_obs')
   
-  print (f'\nRunning LLM pairwise with obs...')
-  predicted_adj_mat_llmpobs = llm_pairwise(VAR_NAMES_AND_DESC[args.dataset], prompts[args.dataset], df, args.temp, include_statistics=True)
-  write_metric(adj_mat, predicted_adj_mat_llmpobs, args.logdir, args.dataset, args.n_samples, f'llm{args.temp}{args.sample_method}_pairwise_with_obs')
+  # print (f'\nRunning LLM pairwise with obs...')
+  # predicted_adj_mat_llmpobs = llm_pairwise(VAR_NAMES_AND_DESC[args.dataset], prompts[args.dataset], df, args.temp, include_statistics=False, include_obs=True)
+  # write_metric(adj_mat, predicted_adj_mat_llmpobs, args.logdir, args.dataset, args.n_samples, f'llm{args.temp}{args.sample_method}_pairwise_with_obs')
+
+elif args.alg == 'llm_stat':
+  print (f'\nRunning LLM BFS with statistics...')
+  predicted_adj_mat_bfsobs = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df, args.temp, include_statistics=True, include_obs=False)
+  write_metric(adj_mat, predicted_adj_mat_bfsobs, args.logdir, args.dataset, args.n_samples, f'llm{args.temp}{args.sample_method}_bfs_with_stat')
+
+  print (f'\nRunning LLM pairwise with statistics...')
+  predicted_adj_mat_llmpobs = llm_pairwise(VAR_NAMES_AND_DESC[args.dataset], prompts[args.dataset], df, args.temp, include_statistics=True, include_obs=False)
+  write_metric(adj_mat, predicted_adj_mat_llmpobs, args.logdir, args.dataset, args.n_samples, f'llm{args.temp}{args.sample_method}_pairwise_with_stat')
 
 elif args.alg == 'llm_only':
   print (f'\nRunning LLM BFS...')
@@ -121,7 +130,7 @@ elif args.alg == 'llm_only':
   write_metric(adj_mat, predicted_adj_mat_bfs, args.logdir, args.dataset, args.n_samples, f'llm{args.temp}_bfs')
 
   print (f'\nRunning LLM pairwise...')
-  predicted_adj_mat_llmp = llm_pairwise(VAR_NAMES_AND_DESC[args.dataset], prompts[args.dataset], df, args.temp, include_statistics=False)
+  predicted_adj_mat_llmp = llm_pairwise(VAR_NAMES_AND_DESC[args.dataset], prompts[args.dataset], df, args.temp)
   write_metric(adj_mat, predicted_adj_mat_llmp, args.logdir, args.dataset, args.n_samples, f'llm{args.temp}_pairwise')
 
 # elif args.alg == 'llm_pairwise_all':
@@ -142,49 +151,5 @@ elif args.alg == 'llm_only':
 #   predicted_adj_mat_llm_mid_obs = midllm_pairwise(VAR_NAMES_AND_DESC[args.dataset], prompts[args.dataset], df, model, tokenizer, max_new_tokens=50, include_statistics=True)
 #   write_metric(adj_mat, predicted_adj_mat_llm_mid_obs, args.logdir, args.dataset, args.n_samples, f'llm_pairwise_{model_name[:5]}_obs')
 
-
-# elif args.alg == 'llm_bfs_all':
-#   # print (f'\nRunning LLM BFS...')
-#   # predicted_adj_mat_bfs = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df)
-#   # write_metric(adj_mat, predicted_adj_mat_bfs, args.logdir, args.dataset, args.n_samples, 'llm_bfs')
-#   # print (f'\nRunning LLM BFS with corr...')
-#   # predicted_adj_mat_bfscorr = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df, include_statistics=True)
-#   # write_metric(adj_mat, predicted_adj_mat_bfscorr, args.logdir, args.dataset, args.n_samples, 'llm_bfs_with_corr')
-#   print (f'\nRunning LLM BFS with obs...')
-#   predicted_adj_mat_bfsobs = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df, include_statistics=True, include_obs=True)
-#   write_metric(adj_mat, predicted_adj_mat_bfsobs, args.logdir, args.dataset, args.n_samples, 'llm_bfs_with_obs')
-
-# elif args.alg == 'ges':
-#   data = data + gaussian_noise
-#   predicted_adj_mat = ges(data, score_func='local_score_BIC', maxP=None, parameters=None)
-# elif args.alg == 'pc':
-#   data = data + gaussian_noise
-#   predicted_adj_mat = pc(data)
-# elif args.alg == 'dagma_nonlinear':
-#   predicted_adj_mat = dagma_nonlinear_wrapper(data, len(adj_mat), args.lambda1, args.lambda2)
-# elif args.alg == 'dagma_linear':
-#   predicted_adj_mat = dagma_linear_wrapper(data, args.lambda1)
-# elif args.alg == 'notears':
-#   predicted_adj_mat = notears(data, lambda1=args.lambda1, loss_type='l2', w_threshold=args.w_threshold)
-# elif args.alg == 'llm_pairwise':
-#   predicted_adj_mat = llm_pairwise(VAR_NAMES_AND_DESC[args.dataset], prompts[args.dataset], df, include_statistics=False)
-# elif args.alg == 'llm_pairwise_with_obs': #with obs
-#   predicted_adj_mat = llm_pairwise(VAR_NAMES_AND_DESC[args.dataset], prompts[args.dataset], df, include_statistics=True)
-# elif args.alg == 'llm_bfs':
-#   predicted_adj_mat = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df, include_statistics=False)
-# elif args.alg == 'llm_bfs_with_statistics':
-#   predicted_adj_mat = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df, include_statistics=True)
-# elif args.alg == 'llm_bfs_with_obs':
-#   predicted_adj_mat = llm_bfs(VAR_NAMES_AND_DESC[args.dataset], args.dataset, df, include_statistics=True, include_obs=True)
 else:
   raise ValueError(f"Unknown algorithm {args.alg}")
-
-# metrics = compute_metrics(adj_mat, predicted_adj_mat)
-# logdir = f"{args.logdir}/{args.dataset}/{args.n_samples}"
-# os.makedirs(logdir, exist_ok=True)
-# logfile = f"{logdir}/{args.alg}" 
-# if args.alg in ['dagma_nonlinear', 'dagma_linear', 'notears']:
-#   logfile += f"_lambda1={args.lambda1}"
-# logfile += ".json"
-# with open(logfile, "w") as outfile: 
-#     json.dump(metrics, outfile)
